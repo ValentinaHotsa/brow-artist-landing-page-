dccd3c0d738fabdfcefa14b9ec4924eac5ca5b04
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2025-02-04 09:41:34 +0100

    create burger menu for header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import 'modern-normalize/modern-normalize.css';
+
+const burgerMenu = document.getElementById('mobileMenu');
+const menuBtn = document.getElementById('menuBtn');
+
+menuBtn.addEventListener('click', function () {
+  burgerMenu.classList.toggle('open');
+  menuBtn.classList.toggle('open');
+});
